6c0bf1f33a1a5889e4fa02b93755196f92a890a7
kpehterev <pexterevkos@mail.ru>
2025-09-10 14:58:48 +0300

    1

---
+++ src/main/java/com/example/testwithcicd/TestwithcicdApplication.java
@@ -0,0 +1,12 @@
+package com.example.testwithcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwithcicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestwithcicdApplication.class, args);
+    }
+}
